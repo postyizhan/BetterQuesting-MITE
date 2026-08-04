76eae82279983b68b6f1b808d7fcdbb4048edb21
驿站 <97342038+postyizhan@users.noreply.github.com>
2026-08-04 17:01:02 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Fish Example Mod
+
+## Setup
+
+For setup instructions please see the [fish documentation page](https://minecraftistooeasy.github.io/pages/docs-navigation.html) that relates to the IDE that you are using.
+
+## License
+
+This template is available under the CC0 license. Feel free to learn from it and incorporate it in your own projects.
+
+Some content refers to Fabric Example Mod
---
+++ src/main/java/com/example/ExampleMod.java
@@ -0,0 +1,33 @@
+package com.example;
+
+import com.example.event.ExampleEvent;
+import net.fabricmc.api.ModInitializer;
+
+import net.xiaoyu233.fml.ModResourceManager;
+import net.xiaoyu233.fml.reload.event.MITEEvents;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ExampleMod implements ModInitializer {
+    public static final String MOD_ID = "modid";
+
+    // This logger is used to write text to the console and the log file.
+    // It is considered best practice to use your mod id as the logger's name.
+    // That way, it's clear which mod wrote info, warnings, and errors.
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+
+        LOGGER.info("Hello MITE Dev world!");
+
+        // Add resource pack domain, default "minecraft"
+        ModResourceManager.addResourcePackDomain(MOD_ID);
+
+        //Register an event listening object
+        ExampleEvent.register();
+    }
+}
